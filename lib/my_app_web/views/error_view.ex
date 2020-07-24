7b058b5225b48b993e533b9cad9020943827ad0a
Serguei Cambour <s.cambour@gmail.com>
2020-07-24 11:53:27 +0200

    Add custom responses for 404 and 500 status codes

--- lib/my_app_web/views/error_view.ex
+++ lib/my_app_web/views/error_view.ex
@@ -1,11 +1,13 @@
 defmodule MyAppWeb.ErrorView do
   use MyAppWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Endpoint not found!"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal server error :("}}
+  end
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
