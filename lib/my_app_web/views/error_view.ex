182c6af817cbba941abba44930b33f48c08f771a
Serguei Cambour <s.cambour@gmail.com>
2020-11-03 13:31:07 +0100

    Move render/2 upper to fix linter warnings

--- lib/my_app_web/views/error_view.ex
+++ lib/my_app_web/views/error_view.ex
@@ -9,14 +9,14 @@ defmodule MyAppWeb.ErrorView do
     %{errors: %{detail: "Internal server error :("}}
   end
 
+  def render("401.json", %{message: message}) do
+    %{errors: %{detail: message}}
+  end
+
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
-  end
-
-  def render("401.json", %{message: message}) do
-    %{errors: %{detail: message}}
   end
 end
